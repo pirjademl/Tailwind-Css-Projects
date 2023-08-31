7eda9bbfc84a28790ba8f5aca49478f9d3b07502
unknown <pirjademl7@gmail.com>
2023-08-31 11:40:32 +0530

    Landing Page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        "customColor":"#DCF5FD",
+        "custom-heading-text":"#1E3264",
+        "fs-secondary":"#677AA1",
+      }
+    },
+  },
+  plugins: [],
+  
+}
+
