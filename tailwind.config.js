91f39b43a03df273e4813888a876bccd99951b19
unknown <pirjademl7@gmail.com>
2023-09-04 15:57:59 +0530

    Added form component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,14 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
-      colors:{
-        "customColor":"#DCF5FD",
-        "custom-heading-text":"#1E3264",
-        "fs-secondary":"#677AA1",
-      }
+      colors: {
+        customColor: "#DCF5FD",
+        "custom-heading-text": "#1E3264",
+        "fs-secondary": "#677AA1",
+        "bg-image": "#E6F8FE",
+        "color-neutral":"#D9F3FD",
+      },
     },
   },
   plugins: [],
-  
-}
-
+};
